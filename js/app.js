1219cdab58a7a9f4e98bf65bd26e1a7d0c02cab4
simrankaur96 <simran0622@gmail.com>
2018-08-22 19:44:03 +0530

    Adding app.js

---
+++ js/app.js
@@ -0,0 +1,24 @@
+(
+  angular.module('mainApp',[])
+  .controller('CINController', function($scope, $http){
+    $scope.display() = function(){
+      $scope.displayContent = false;
+      $scope.userError = false;
+      $http.get("https://cors.io/?https://99sszhzro1.execute-api.us-east-1.amazonaws.com/customers/info?cin="+
+                $scope.CINinput).then(function(response){
+                  if(response.data.queryStatus!="success"){
+                    $scope.userError = true;
+                    $scope.errorResponse = response.data;
+                  }else{
+                    if(response.data.customerType=="Corporate"){
+                      $scope.type = true;
+                    }else{
+                      $scope.type = false;
+                    }
+                    $scope.displayContent = true;
+                    $scope.user = response.data;
+                  }
+                });
+    }
+  });
+)();
