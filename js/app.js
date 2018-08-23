23a08d1e15a536a0471aa8f401f6f39c71589cc7
simrankaur96 <simran0622@gmail.com>
2018-08-23 21:26:34 +0530

    Fixed CIN picker for enter submit and numeric value input

--- js/app.js
+++ js/app.js
@@ -2,23 +2,29 @@
   angular.module('mainApp',[])
   .controller('CINController', function($scope, $http){
     $scope.display = function(){
-      $scope.displayContent = false;
-      $scope.userError = false;
-      $http.get("https://cors.io/?https://l20jgb1jch.execute-api.us-east-1.amazonaws.com/customers/info?cin="+
-                $scope.CINinput).then(function(response){
-                  if(response.data.queryStatus!="success"){
-                    $scope.userError = true;
-                    $scope.errorResponse = response.data.queryStatus;
-                  }else{
-                    if(response.data.customerType=="Corporate"){
-                      $scope.type = true;
+      var cin = document.getElementById("CINinput").value;
+      if(!isNaN(cin)){
+        $scope.displayContent = false;
+        $scope.userError = false;
+        $http.get("https://cors.io/?https://l20jgb1jch.execute-api.us-east-1.amazonaws.com/customers/info?cin="+
+                  cin).then(function(response){
+                    if(response.data.queryStatus!="success"){
+                      $scope.userError = true;
+                      $scope.errorResponse = response.data.queryStatus;
                     }else{
-                      $scope.type = false;
+                      if(response.data.customerType=="Corporate"){
+                        $scope.type = true;
+                      }else{
+                        $scope.type = false;
+                      }
+                      $scope.displayContent = true;
+                      $scope.user = response.data;
                     }
-                    $scope.displayContent = true;
-                    $scope.user = response.data;
-                  }
-                });
+                  });
+        }else{
+          $scope.userError = true;
+          $scope.errorResponse = "Please enter valid CIN number";
+        }
     }
   });
 })();
