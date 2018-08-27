7c489adcc555ea23bd4acc38c26390c13c1f4901
simrankaur96 <simran0622@gmail.com>
2018-08-27 12:10:25 +0530

    Added updated Supply Chain UI

--- js/app.js
+++ js/app.js
@@ -153,6 +153,50 @@
                         var chart = new google.visualization.PieChart(document.getElementById('piechart'));
                         chart.draw(data, options);
                     }
+
+                    //retail banking
+                    var acc1;
+                    var responseCallback1 = function (response) {
+                        $scope.data1 = response.data;
+
+                        s1=$scope.data1;
+                        $scope.accno=s1.Items[0].ACCOUNT_NUMBER.S;
+                        acc1=s1.Items[0].ACCOUNT_NUMBER.S;
+                        $scope.bal=s1.Items[0].CURRENT_BALANCE.N;
+                        var s1;
+
+                    var l2='https://cwoqef7rh1.execute-api.ap-south-1.amazonaws.com/Transactions/transactions/'+acc1;
+                    $http.get(l2).then(responseCallback2);
+
+
+                    }
+
+                    var l1='https://80a0rk0mlk.execute-api.ap-south-1.amazonaws.com/Accounts/accounts/'+cin;
+                    $http.get(l1).then(responseCallback1);
+
+
+
+                    var responseCallback2 = function (response) {
+                        $scope.data2 = response.data;
+
+                         var datac = [];
+                         var vcredit=0;
+                         var vdebit=0;
+                         var s2=$scope.data2;
+
+                         for (var i = 0; i < s2.Items.length; i++) {
+                             if(s2.Items[i].TRANSACTION_TYPE.S == "CREDIT")
+                             vcredit =vcredit+(+s2.Items[i].TRANSACTION_AMOUNT.N);
+                             else if(s2.Items[i].TRANSACTION_TYPE.S == "DEBIT")
+                             vdebit =vdebit+(+s2.Items[i].TRANSACTION_AMOUNT.N);
+                         }
+                         datac.push(vcredit);
+                         datac.push(vdebit);
+                         f1(datac);
+                         $scope.creditval=vcredit;
+                         $scope.debitval=vdebit;
+                        }
+
         }else{
           $scope.userError = true;
           $scope.errorResponse = "Please enter valid CIN number";
---
+++ js/retailBanking/chartfun1.js
@@ -0,0 +1,42 @@
+function f1(datac) {
+
+    var ctx1 = $("#pie-chartcanvas-1");
+
+    var data1 = {
+        labels: ["Credit","Debit"],
+        datasets: [
+            {
+                label: "NETVAL",
+                data: datac,
+                backgroundColor: [
+                "#c0c0c0","#14366E"
+                ],
+                borderWidth: [1, 1]
+            }
+        ]
+    };
+
+
+    var options = {
+        title: {
+            display: true,
+            position: "top",
+            text: "CREDIT vs DEBIT",
+            fontSize: 18,
+            fontColor: "#111"
+        },
+        legend: {
+            display: true,
+            position: "bottom"
+        }
+    };
+
+
+    var chart1 = new Chart(ctx1, {
+        type: "doughnut",
+        data: data1,
+        options: options
+    });
+
+
+};
