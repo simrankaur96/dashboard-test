e3241b8b6065c7ac0637476d8401bd9652bcef48
garginator <garginator@gmail.com>
2018-08-29 11:56:40 +0530

    Update app.js

--- TradeFinance/app.js
+++ TradeFinance/app.js
@@ -1,7 +1,6 @@
 tradeFinance = function(cin,$scope,$http){
-  $scope.status_customer=true;
+  $scope.status_customer = true;
   var userDetailsSuccess = function (response) {
-    $scope.displayTF=true;
     var wip=0;
     var te=0;
     $scope.userDetails = response.data;
@@ -9,8 +8,9 @@ tradeFinance = function(cin,$scope,$http){
     te=te+$scope.userDetails.message['Total Exposure'];
     if($scope.userDetails.message['status']=="Success")
     {
-        $scope.status_customer=true;  
+        $scope.status_customer=true;
         drawChart(wip,te);
+        
         //$scope.nocus=false;
     }
     else
@@ -21,19 +21,19 @@ tradeFinance = function(cin,$scope,$http){
     }
   };
   var userDetailsError = function (error) {
-      console.log("ERROR :::" + error);
-      console.log(error)
+      //console.log("ERROR :::" + error);
+      //onsole.log(error)
   }
-      $scope.displayTF = false;
+
       $http.get("https://bztwi1cu97.execute-api.ap-south-1.amazonaws.com/TF/tf?CIN="+cin)
       .then(userDetailsSuccess,userDetailsError);
       google.charts.load('current', { 'packages': ['corechart'] });
       google.charts.setOnLoadCallback(drawChart);
 
-      // Draw the chart and set the chart values
       
 }
-function drawChart(x,y) {
+// Draw the chart and set the chart values
+      function drawChart(x,y) {
 
           var data = google.visualization.arrayToDataTable([
               ['Task', 'Hours per Day'],
@@ -43,15 +43,15 @@ function drawChart(x,y) {
           ]);
 
           // Optional; add a title and set the width and height of the chart
-          //var options = {  'width': '100%', 'height': 250,is3D: true};
           var options = {
               chartArea:{top:0, bottom:30},
               'legend': { position:'bottom'},
               'fontSize':12,
-             'width':400,
-           'height': 250,
+              'width':400,
+              'height': 250,
               is3D: true
           };
+
           // Display the chart inside the <div> element with id="piechart"
           var chart = new google.visualization.PieChart(document.getElementById('piechart'));
           chart.draw(data, options);
